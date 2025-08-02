ae51d442aab0c7308a577fcb8f2c89d18c01e812
GoJira56 <jirawatsingkam766@gmail.com>
2025-08-02 17:43:23 +0700

    Update script.java

--- script.java
+++ script.java
@@ -1,3 +1,9 @@
 let "A" = document.getElementbyId ("meta")
+let "c1" = document.getElementbyId ("c1")
+let "c2" = document.getElementbyId ("c2")
+let "c3" = document.getElementbyId ("c3")
+let "c4" = document.getElementbyId ("c4")
+let "c5" = document.getElementbyId ("c5")
+let "c6" = document.getElementbyId ("c6")
 
 meta.addEventlistener("click", = () {meta.innerHTML ="Random"      })
